a8ca1f322da0b066e1f2d141d06139c0b4efe879
Diego Villafuerte <Diego Villafuerte>
2016-07-15 14:52:40 -0400

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,224 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class GameManager : MonoBehaviour {
+    public GameObject thingWithTapScript;
+    public GameObject beeScript;
+    public GameObject openBoxAnimation;
+
+    AudioSource anxietyRating;
+    AudioSource stimulusRating;
+
+    AudioSource[] aSources;
+
+    public bool aIsPlaying, sIsPlaying = false;
+
+    DateTime oldDate;
+    DateTime currentDate;
+    public bool setOldDate = true;
+    int minutes;
+    int currentMin;
+    bool hasPlayedOnce = false;
+
+    public int level = 0;
+
+    // Use this for initialization
+    void Start () {
+        //gets the audio sources
+        aSources = GetComponents<AudioSource>();
+
+        //sets the audio sources
+        anxietyRating = aSources[0];
+        stimulusRating = aSources[1];
+
+        PlayAnxietyRating();
+
+        //one more to add for the "try again" recording
+    }
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if there's no rating message playing
+        if (!anxietyRating.isPlaying && !stimulusRating.isPlaying)
+        {
+            //controls what we do each level
+            switch (level)
+            {
+                case 0:
+                    //if we've created the box
+                    if (setOldDate == true && thingWithTapScript.GetComponent<TapToPlaceParent>().firstPass == false)
+                    {
+                        //start calulating time since this moment
+                        oldDate = System.DateTime.Now;
+                        //move on
+                        level++;
+                    }
+                    break;
+                case 1:
+                    handleLevels();
+                    break;
+                case 2:
+                    handleLevels();
+                    break;
+                case 3:
+                    handleLevels();
+                    break;
+
+
+
+            }
+        }
+        //update minutes
+        currentDate = System.DateTime.Now;
+        //number of minutes since box was created
+        minutes = currentDate.Minute - oldDate.Minute;
+
+    }
+
+
+    public void handleLevels()
+    {
+        //each time the minute increments we ask for the subejects anxiety rating one time, for a total of 5 minutes
+        switch (minutes)
+        {
+            case 1:               
+                handleMinutes();
+                break;
+            case 2:
+                handleMinutes();
+                break;
+            case 3:
+                handleMinutes();
+                break;
+            case 4:
+                handleMinutes();
+                break;
+            case 5:
+                handleMinutes();
+                //Turn off the box and prepare for the next stage
+                TurnOffBox();
+                break;
+        }
+
+
+
+        //
+        //
+        //
+        //Add switch statement for different animal types - mouse, bee, spider
+        //
+        //
+        //
+        //
+        if (level == 1)
+        {
+        }
+        else if (level == 2)
+        {
+            openBoxAnimation.GetComponent<Animator>().SetTrigger("Open");
+            beeScript.GetComponent<MouseAI>().cWaypoint = 0;
+            beeScript.GetComponent<MouseAI>().currentWaypoints = beeScript.GetComponent<MouseAI>().waypointsIntermediate;
+            beeScript.GetComponent<MouseAI>().currentWaypoints[beeScript.GetComponent<MouseAI>().cWaypoint].gameObject.tag = "intermediate current";
+            beeScript.GetComponent<MouseAI>().diff = LevelState.State.INTERMEDIATE;
+            beeScript.GetComponent<MouseAI>().firstCollision = true;
+        }
+        else if (level == 3)
+        {
+            beeScript.GetComponent<MouseAI>().cWaypoint = 0;
+            beeScript.GetComponent<MouseAI>().currentWaypoints = beeScript.GetComponent<MouseAI>().waypointsAdvanced;
+            beeScript.GetComponent<MouseAI>().currentWaypoints[beeScript.GetComponent<MouseAI>().cWaypoint].gameObject.tag = "advanced current";
+            beeScript.GetComponent<MouseAI>().diff = LevelState.State.ADVANCED;
+            beeScript.GetComponent<MouseAI>().firstCollision = true;
+        }
+
+
+        //if (level == 1)
+        //{
+        //}
+        //else if (level == 2)
+        //{
+        //    openBoxAnimation.GetComponent<Animator>().SetTrigger("Open");
+        //    beeScript.GetComponent<beeAI>().cWaypoint = 0;
+        //    beeScript.GetComponent<beeAI>().currentWaypoints = beeScript.GetComponent<beeAI>().waypointsIntermediate;
+        //    beeScript.GetComponent<beeAI>().currentWaypoints[beeScript.GetComponent<beeAI>().cWaypoint].gameObject.tag = "intermediate current";
+        //    beeScript.GetComponent<beeAI>().diff = LevelState.State.INTERMEDIATE;
+        //    beeScript.GetComponent<beeAI>().firstCollision = true;
+        //}
+        //else if (level == 3)
+        //{
+        //    beeScript.GetComponent<beeAI>().cWaypoint = 0;
+        //    beeScript.GetComponent<beeAI>().currentWaypoints = beeScript.GetComponent<beeAI>().waypointsAdvanced;
+        //    beeScript.GetComponent<beeAI>().currentWaypoints[beeScript.GetComponent<beeAI>().cWaypoint].gameObject.tag = "advanced current";
+        //    beeScript.GetComponent<beeAI>().diff = LevelState.State.ADVANCED;
+        //    beeScript.GetComponent<beeAI>().firstCollision = true;
+        //}
+
+
+
+    }
+
+    //if recording has not yet been played this minute, play it
+    void handleMinutes()
+    {
+        //if the recording has been played and the previous minute is not the current minute
+        if (hasPlayedOnce && currentMin != minutes)
+        {
+            hasPlayedOnce = false;
+        }
+        //if the anxiety rating has not been played this minute
+        if (!hasPlayedOnce)
+        {
+
+            //play it
+            PlayAnxietyRating();
+            //it has now been played
+            hasPlayedOnce = true;
+            //record what minute it was played
+            currentMin = minutes;
+        }
+
+    }
+
+
+    void TurnOffBox()
+    {
+        //if the anxiety rating message is done playing
+        if (!anxietyRating.isPlaying && !stimulusRating.isPlaying)
+        {
+            //allow us to go to the next level
+            level++;
+            //reset the minute counter so this is not called more than once
+            oldDate = System.DateTime.Now;
+            //play the stimulus rating message
+            PlayStimulusRating();
+
+            //turn off the box
+            ////
+            //
+            //
+            //
+            //
+            //
+        }
+
+    }
+
+
+    //plays the anxiety rating message
+    public void PlayAnxietyRating()
+    {
+        if (!anxietyRating.isPlaying && !stimulusRating.isPlaying)
+            anxietyRating.Play();
+    }
+    //plays the stimulus rating message
+    public void PlayStimulusRating()
+    {
+        if (!anxietyRating.isPlaying && !stimulusRating.isPlaying)
+            stimulusRating.Play();
+    }
+
+}
---
+++ Assets/Scripts/VoiceManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.Windows.Speech;
+
+public class VoiceManager : MonoBehaviour {
+
+    //Internal reference to a keywordRecognizer.
+    public bool toContinue = false;
+    private string response;
+    private string[] oneToOneHundred = new string[100];
+    private ArrayList responseDirectory = new ArrayList();
+    private KeywordRecognizer kr;
+
+    // Use this for initialization
+    void Start () {
+        createString();
+
+		//Create a new keywordRecognizer, with the words from one to one hundred
+		kr = new KeywordRecognizer(oneToOneHundred);
+
+		//Register OnVoiceCommand to kr's onPhraseRecognized
+		kr.OnPhraseRecognized += OnVoiceCommand;
+    }
+    public void startListening()
+    {
+        kr.Start();
+    }
+    public void stopListening()
+    {
+        kr.Stop();
+    }
+
+    //when a keyword is recognized
+	private void OnVoiceCommand(PhraseRecognizedEventArgs args)
+	{
+        //if the keyword is anything but "try again"...
+        if(!args.text.Equals("try again"))
+        {
+            //Go to the next level
+            toContinue = true;
+            //add the responses and confidence levels to their arraylists
+            addToResponseDirectory(args.text);
+        }
+        //otherwise...
+        else
+        {
+            //keyword was "try again", so we wait for a new response
+            toContinue = false;
+        }
+	}
+
+    //creates an array of strings that has the numbers "one" to "one-hundred"
+    private void createString()
+    {
+        for (int i = 0; i < oneToOneHundred.Length; i++)
+        {
+            oneToOneHundred[i] = i.ToString();
+        }
+    }
+    //it doesn't currently deal with the "try again" situation, I guess I can just add it to the end of the oneToOnehundredArray..
+
+    private void addToResponseDirectory(string toAdd)
+    {
+        //adds string to arrayList
+        responseDirectory.Add(toAdd);
+    }
+
+    private void showList()
+    {
+        //put responseDirectory on the screen for the psychologist to record.
+    }
+}
