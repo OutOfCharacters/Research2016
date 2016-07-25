1770e583e1c1c86f37bf8470d9cf126528f9870f
drv14 <drv14@case.edu>
2016-07-25 15:33:18 -0400

    Add files via upload
    add counterforceobject for wall avoidance

---
+++ Assets/Scripts/counterForceObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class counterForceObject{
+
+    float counterForce;
+    float wallNumber;
+
+    public counterForceObject(float cf, float wn)
+    {
+        counterForce = cf;
+        wallNumber = wn;
+    }
+    public float getCF()
+    {
+        return counterForce;
+    }
+    public float getWN()
+    {
+        return wallNumber;
+    }
+    public void setCF(float cf)
+    {
+        counterForce = cf;
+    }
+    public void setWN(float wn)
+    {
+        wallNumber = wn;
+    }
+	
+}
